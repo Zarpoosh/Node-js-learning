94cf9a6d44fd0140d8685f6483d896139a422c94
Zarpoosh <mzar67839@gmail.com>
2025-11-26 17:58:25 +0330

    create server and rout

--- 07/app.js
+++ 07/app.js
@@ -7,9 +7,19 @@ const app = express();
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./view/index.html", {root:__dirname});
+  res.sendFile("./view/index.html", { root: __dirname });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./view/about.html", {root:__dirname});
+  res.sendFile("./view/about.html", { root: __dirname });
+});
+
+// redirect
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 page  : دقیقا مثل 
+app.use((req, res) => {
+  res.sendFile("./view/404.html", { root: __dirname });
 });
