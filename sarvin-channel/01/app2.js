5f6a40a0a1086e9e875786874726a36f466c7a56
Zarpoosh <mzar67839@gmail.com>
2026-02-11 20:46:42 +0330

    add post method

--- sarvin-channel/01/app2.js
+++ sarvin-channel/01/app2.js
@@ -9,11 +9,18 @@
 const express=require("express")
 require("dotenv").config()
 const app=express()
+app.use(express.json())
 // app.get()
 // app.post()
 // app.put()
 // app.delete()
 
+
+const courses=[
+    {id:1, name:"html"},
+    {id:2, name:"css"},
+    {id:3, name:"py"},
+]
 
 app.get("/", (req, res)=>{
     res.send("hello minicode :)")
@@ -24,10 +31,31 @@ app.get("/api/courses", (res,req)=>{
     res.send(["html" , "css" , "js"])    
 })
 
-app.get("/api/courses/:id /:name?", (res,req)=>{
-    res.send([req.params.id ,req.params.name , req.query.sort])   
+
+// /api/courses/:id /:name?=id
+// app.get("/api/courses/:id /:name?", (res,req)=>{
+//     res.send([req.params.id ,req.params.name , req.query.sort])   
+// })
+
+app.get("/api/courses/:id ", (res,req)=>{
+   const course= courses.find(c=> c.id ===parseInt(req.params.id))
+   if(!course) res.status(404).send("course with given id not found ")
+    res.send(course)   
 })
 
+// post
+app.post("/api/courses",(res,req)=>{
+if (!req.body.name || req.body.name.length <3 ) {
+    req.status(404).send("name is requires !")
+}
+
+    const course={
+        id:course.length +1 , 
+        name:req.body.name
+    }
+    course.push(course)
+    res.send(course)
+})
 
 const port=process.env.APP_PORT ||3000
 app.listen(port, ()=>{
