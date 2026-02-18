6af5dee27069f731e3cad1af87412b17fe845c28
Zarpoosh <mzar67839@gmail.com>
2026-02-18 20:17:25 +0330

    add middleware in app2.js file

--- sarvin-channel/01/app2.js
+++ sarvin-channel/01/app2.js
@@ -6,15 +6,56 @@
 // console.log(uuid.v4());
 
 // ---------------------------------------------------------
+const logger=require("./logger")
+const logger2=require("./logger")
+const helmet=require("helmet")
+const morgan=require("morgan")
 const express=require("express")
 require("dotenv").config()
+const startupdebug=require("debug")("startup")
+const dbdebug=require("debug")("db")
 const app=express()
+
+// ! middleware
 app.use(express.json())
+// built in middleware
+app.use(express.static("public"))
+app.use(helmet())
+
+// نوشتن کدی که تشحیص بده تو کدوم محیط اجرا هست 
+if (app.get("env")==="development") {
+    app.use(morgan("tiny"))
+}
+
+// denug middleware : برای جلوگیری از نوشتن و پاک کردن یه عالمه کنسول لاگ 
+startupdebug("hello minoo...")
+
+
+
+// create middleware
+// app.use(function(req, res, next){
+//     console.log("logging");
+//     next()
+// })
+
+// app.use((req,res,next)=>{
+//     console.log("authentication...");
+//     next()
+// })
+
+
+
 // app.get()
 // app.post()
 // app.put()
 // app.delete()
 
+// create a middleware
+app.use(logger)
+// یه سری دیتا ها که به api ارسال میشه با فرمت قدیمی ارسلا شه 
+app.use(express.urlencoded({extended:true}))
+
+app.use(logger2)
 
 const courses=[
     {id:1, name:"html"},
@@ -23,11 +64,12 @@ const courses=[
 ]
 
 app.get("/", (req, res)=>{
+    dbdebug("hello from minoo dbdebug....")
     res.send("hello minicode :)")
 })
 
 
-app.get("/api/courses", (res,req)=>{
+app.get("/api/courses", (req,res)=>{
     res.send(["html" , "css" , "js"])    
 })
 
@@ -37,14 +79,14 @@ app.get("/api/courses", (res,req)=>{
 //     res.send([req.params.id ,req.params.name , req.query.sort])   
 // })
 
-app.get("/api/courses/:id ", (res,req)=>{
+app.get("/api/courses/:id ", (req,res)=>{
    const course= courses.find(c=> c.id ===parseInt(req.params.id))
    if(!course) res.status(404).send("course with given id not found ")
     res.send(course)   
 })
 
 // post
-app.post("/api/courses",(res,req)=>{
+app.post("/api/courses",(req,res)=>{
 if (!req.body.name || req.body.name.length <3 ) {
     req.status(404).send("name is requires !")
     return
@@ -59,7 +101,7 @@ if (!req.body.name || req.body.name.length <3 ) {
 })
 
 
-app.put("/api/courses/:id", (res,req)=>{
+app.put("/api/courses/:id", (req,res)=>{
     const course= courses.find(c=> c.id ===parseInt(req.params.id))
     if(!course) return res.status(404).send("course with given id not found ")
         if (!req.body.name || req.body.name.length <3 )
@@ -71,7 +113,7 @@ app.put("/api/courses/:id", (res,req)=>{
 
 
 
-app.delete("/api/courses/:id", (res,req)=>{
+app.delete("/api/courses/:id", (req,res)=>{
     const course= courses.find(c=> c.id ===parseInt(req.params.id))
     if(!course) return res.status(404).send("course with given id not found ")
 
--- sarvin-channel/01/logger.js
+++ sarvin-channel/01/logger.js
@@ -1,10 +1,22 @@
-var name="minicode"
+// var name="minicode"
 
 
-const  log=(data)=>{
-    console.log(data);
+// const  log=(data)=>{
+//     console.log(data);
+// }
+
+
+
+// --------------------------------------create middle ware
+function log (res, req, next) {
+    console.log("logging....");
+    next()
 }
 
 
-
+const log2=(res, req,next)=>{
+    console.log("authuntacation...");
+    next()
+}
 module.exports=log
+module.exports=log2
